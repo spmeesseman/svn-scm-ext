44cecbc9d64476117f2cf1021fea783acd897e63
spmeesseman <spmeesseman@gmail.com>
2019-03-20 11:02:19 -0400

    fix: broken code coverage in azure ci build

--- src/test/index.ts
+++ src/test/index.ts
@@ -10,14 +10,28 @@
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
 
-import * as testRunner from 'vscode/lib/testrunner';
+import * as IstanbulTestRunner from "./istanbultestrunner";
 
-// You can directly control Mocha options by configuring the test runner below
-// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options
-// for more info
-testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
-});
+const testRunner = IstanbulTestRunner;
+
+const mochaOpts: Mocha.MochaOptions = {
+  ui: "tdd", // the TDD UI is being used in extension.test.ts (suite, test, etc.)
+  useColors: true, // colored output from test results,
+  timeout: 10000, // default timeout: 10 seconds
+  retries: 1,
+  reporter: "mocha-junit-reporter",
+  reporterOptions: {
+    mochaFile: __dirname + "/../../test-reports/extension_tests.xml",
+    suiteTitleSeparatedBy: ": "
+  }
+};
+
+testRunner.configure(
+  mochaOpts,
+  // Coverage configuration options
+  {
+    coverConfig: "../../coverconfig.json"
+  }
+);
 
 module.exports = testRunner;
